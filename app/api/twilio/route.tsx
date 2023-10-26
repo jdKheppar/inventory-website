b16577c3f5888dd63478c4f06f1d8d132b0640cc
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-10-26 13:55:35 +0500

    removed the post used handler

--- app/api/twilio/route.tsx
+++ app/api/twilio/route.tsx
@@ -1,30 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import twilio from 'twilio';
 
-const client = twilio(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const { Body, From } = req.body;
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    const twiml = new twilio.twiml.MessagingResponse();
+        // Handle the Twilio message and reply as needed
+        const reply = 'Thank you for your message!'; // Replace with your logic
 
-    // Check if the request is a Twilio message
-    if (req.body && req.body.From) {
-        const messageBody = req.body.Body;
-        const fromNumber = req.body.From;
-
-        // Handle the incoming message
-        console.log(`Received message from ${fromNumber}: ${messageBody}`);
-
-        // Your logic for processing the incoming message and forming a reply
-        const reply = 'Thank you for your message!';
-
-        // Send a reply
-        twiml.message(reply);
+        const twiml = `
+      <Response>
+        <Message>${reply}</Message>
+      </Response>
+    `;
 
         res.setHeader('Content-Type', 'text/xml');
-        res.status(200).send(twiml.toString());
+        res.status(200).end(twiml);
     } else {
         res.status(400).end();
     }
