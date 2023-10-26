8443ccd90a3cc79e59a17cc2cfae33fbb3e54b76
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-10-26 13:44:49 +0500

    changed twilio api to nextjs syntax

--- app/api/twilio/route.tsx
+++ app/api/twilio/route.tsx
@@ -1,15 +1,12 @@
-import twilio from "twilio";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import twilio from 'twilio';
 
 const client = twilio(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_AUTH_TOKEN
 );
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     const twiml = new twilio.twiml.MessagingResponse();
 
     // Check if the request is a Twilio message
@@ -21,13 +18,13 @@ export default async function handler(
         console.log(`Received message from ${fromNumber}: ${messageBody}`);
 
         // Your logic for processing the incoming message and forming a reply
-        const reply = "Thank you for your message!";
+        const reply = 'Thank you for your message!';
 
         // Send a reply
         twiml.message(reply);
 
-        res.writeHead(200, { "Content-Type": "text/xml" });
-        res.end(twiml.toString());
+        res.setHeader('Content-Type', 'text/xml');
+        res.status(200).send(twiml.toString());
     } else {
         res.status(400).end();
     }
