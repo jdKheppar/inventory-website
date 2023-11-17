8edd777ad6e9aba5b9d4a5e57cdebfa6b2178fed
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-17 15:59:39 +0500

    testing nextApirequest

--- app/api/twilioapi/route.ts
+++ app/api/twilioapi/route.ts
@@ -1,24 +1,21 @@
 // pages/api/sms.ts
 import MessagingResponse from 'twilio/lib/twiml/MessagingResponse';
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
+import { NextApiRequest } from 'next';
 
-
-export async function POST(request: any) {
+export async function POST(request: NextApiRequest) {
     try {
         // Fetch the message from the request body
         const { Body } = await request.body;
-        const contentType = request.headers['content-type'];
         // Create reply
         const twiml = new MessagingResponse();
-        twiml.message(`You said: ${contentType}`);
-
+        twiml.message(`You said: ${Body}`);
         return new NextResponse(twiml.toString(), {
             status: 200,
             headers: { 'Content-Type': 'application/xml' }, // TwiML should be served as XML
         });
     } catch (error) {
         console.error('Error', error);
-
         return new NextResponse('Internal server error', { status: 500 });
     }
 }
