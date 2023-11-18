b139c69736ed1ad62368e8a10219f0679905028f
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-18 19:54:42 +0500

    any next request will be entertained

---
+++ app/api/twilioapi/route.ts
@@ -0,0 +1,20 @@
+import MessagingResponse from "twilio/lib/twiml/MessagingResponse";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function POST(req: any) {
+  try {
+    const message = await req.body;
+    console.log(message);
+    const twiml = new MessagingResponse();
+    twiml.message(`You said: ${message.Body}`);
+
+    return new NextResponse(twiml.toString(), {
+      status: 200,
+      headers: { "Content-Type": "application/xml" },
+    });
+  } catch (error) {
+    console.error("Error", error);
+
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
