7e94ae5b5fa93b87be512cb6b7fc90edebd629f8
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-18 19:37:29 +0500

    await req.Body after inspecting request

--- app/api/twilioapi/route.ts
+++ app/api/twilioapi/route.ts
@@ -4,10 +4,10 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function POST(req: NextRequest) {
 
     try {
-        console.log("request is", req);
-
+        const Body = await req.body;
+        console.log(Body);
         const twiml = new MessagingResponse();
-        twiml.message(`You said: ${req}`);
+        twiml.message(`You said: ${Body}`);
 
         return new NextResponse(twiml.toString(), {
             status: 200,
@@ -19,4 +19,6 @@ export async function POST(req: NextRequest) {
         return new NextResponse('Internal server error', { status: 500 });
     }
 }
+
+
 
