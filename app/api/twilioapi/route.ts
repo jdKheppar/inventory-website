6ed68231f0c50b3049265a4d7c054ca99a1c36ea
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-18 10:48:29 +0500

    checking header before parsing body

--- app/api/twilioapi/route.ts
+++ app/api/twilioapi/route.ts
@@ -1,26 +1,41 @@
-
 import MessagingResponse from 'twilio/lib/twiml/MessagingResponse';
 import { NextResponse, NextRequest } from 'next/server';
-import { NextApiRequest } from 'next';
 
 export async function POST(request: NextRequest) {
     try {
-        // Fetch the message from the request body
-        const Body = request.body;
-        // Create reply
-        const twiml = new MessagingResponse();
-        twiml.message(`You said: ${Body}`);
-        return new NextResponse(twiml.toString(), {
-            status: 200,
-            headers: { 'Content-Type': 'application/xml' }, // TwiML should be served as XML
-        });
+        const contentType = request.headers.get('content-type');
+
+        if (contentType === 'application/json') {
+            const { Body } = await request.json();
+
+            // Create reply for JSON request
+            const twiml = new MessagingResponse();
+            twiml.message(`You said (JSON): ${Body}`);
+
+            return new NextResponse(twiml.toString(), {
+                status: 200,
+                headers: { 'Content-Type': 'application/xml' }, // TwiML should be served as XML
+            });
+
+        } else if (contentType === 'application/xml') {
+            const body = await request.text();
+
+            // Create reply for XML request
+            const twiml = new MessagingResponse();
+            twiml.message(`You said (XML): ${body}`);
+
+            return new NextResponse(twiml.toString(), {
+                status: 200,
+                headers: { 'Content-Type': 'application/xml' }, // TwiML should be served as XML
+            });
+
+        } else {
+            return new NextResponse('Unsupported content type', { status: 415 });
+        }
+
     } catch (error) {
         console.error('Error', error);
+
         return new NextResponse('Internal server error', { status: 500 });
     }
 }
-
-
-
-
-
