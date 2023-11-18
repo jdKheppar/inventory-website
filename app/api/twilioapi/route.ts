58ceba6d73e3d16ce19635569928d31b78911d94
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-18 19:19:35 +0500

    after adding some console logs

--- app/api/twilioapi/route.ts
+++ app/api/twilioapi/route.ts
@@ -5,9 +5,11 @@ export async function POST(req: any) {
 
     try {
         const message = req.Parameters;
-
+        console.log("req is: ", req);
+        console.log("req.Body is: ", req.Body);
+        console.log("req.Parameters is: ", message);
         const twiml = new MessagingResponse();
-        twiml.message(`You said: ${message.Body}`);
+        twiml.message(`You said: ${req.Parameters.Body}`);
 
         return new NextResponse(twiml.toString(), {
             status: 200,
