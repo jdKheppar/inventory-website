235b0f446290391dcc8519564b30d892cb810fc6
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-18 22:18:35 +0500

    req.body after viewing some syntax

--- app/api/twilioapi/route.ts
+++ app/api/twilioapi/route.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: any) {
   try {
-    const message = await req.json();
+    const message = await req.body;
     console.log("message(req.json) is:", message);
     console.log("message.parameters is:", message.parameters);
     console.log("message.body is:", message.Body);
@@ -14,7 +14,8 @@ export async function POST(req: any) {
       status: 200,
       headers: { "Content-Type": "application/xml" },
     });
-  } catch (error) {
+  }
+  catch (error) {
     console.error("Error", error);
 
     return new NextResponse("Internal server error", { status: 500 });
